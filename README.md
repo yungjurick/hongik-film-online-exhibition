13f961615f34f8df4161aa65cc312e2c423a5ff1
Yung Ju Rick Kim <yungjurick@jaranda.kr>
2020-12-19 23:27:20 +0900

    init project with nuxt

---
+++ README.md
@@ -0,0 +1,32 @@
+# 홍익대학교 영화전공 온라인 졸업상영회
+#### Hongik Univ. Movie Major Online Exhibition
+
+<img src="https://img.shields.io/badge/vuejs%20-%2335495e.svg?&style=for-the-badge&logo=vue.js&logoColor=%234FC08D"/>
+<img src="https://img.shields.io/badge/firebase%20-%23039BE5.svg?&style=for-the-badge&logo=firebase"/>
+
+이 프로젝트는 홍익대학교 영화전공 온라인 졸업상영회 소스코드 입니다. 해당 프로젝트에 대한 모든 저작권은 홍익대학교 영화전공 졸업준비위원회에 있습니다.
+
+해당 프로젝트는 Nuxt 바탕으로 제작된 정적 사이트이며 Netlify를 통해 호스팅하고 있습니다. 추가적으로, 정적 데이터 외로는 Firebase를 통해 데이터 관리 및 보관을 하고 있습니다.
+
+This project is the source code for Hongik University Movie Major's Online Exhibiton. All of the source code and the project as its entirety are owned by Hongik University movie major graduation preparation committee.
+
+The project is created using Nuxt and the project is hosted using Netlify. Aside from the static data, this project also utilizes Firebase as a method to manage and contain other additional dynamic data.
+
+#### Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ components/Toolbar.vue
@@ -0,0 +1,79 @@
+<template>
+  <header class="toolbar">
+    <div class="toolbar__logo" @click="navigateToHome()">
+      <img src="@/assets/img/hgff_logo.png" alt="logo">
+      <p>
+        2020 홍익대학교 영상애니메이션전공<br>
+        졸업작품 상영회
+      </p>
+    </div>
+    <div class="toolbar__nav">
+      <NuxtLink class="toolbar__nav__link" to="/flashback/" active-class="toolbar__nav__link--active">FLASHBACK</NuxtLink>
+      <NuxtLink class="toolbar__nav__link" to="/2020/" active-class="toolbar__nav__link--active">2020</NuxtLink>
+      <NuxtLink class="toolbar__nav__link" to="/profile/" active-class="toolbar__nav__link--active">PROFILE</NuxtLink>
+      <NuxtLink class="toolbar__nav__link" to="/guest/" active-class="toolbar__nav__link--active">GUEST</NuxtLink>
+    </div>
+    <!-- <nav class="nav">
+      <NuxtLink class="nav__link" to="/">Home</NuxtLink>
+      <NuxtLink class="nav__link" to="/about/">About</NuxtLink>
+    </nav> -->
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+      if (this.$route.path !== '/') {
+        this.$router.push('/');
+      } else {
+        this.$router.go()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .toolbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 25px 0;
+    &__logo {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      & > img {
+        height: 46px;
+        width: 52px;
+        object-fit: contain;
+        margin-right: 8px;
+      }
+      & > p {
+        font-size: 17px;
+        font-weight: 600;
+        line-height: 1.35;
+        letter-spacing: -0.5px;
+        color: #d0d0d0;
+      }
+    }
+    &__nav {
+      &__link {
+        font-size: 16px;
+        font-weight: 900;
+        line-height: 1;
+        letter-spacing: normal;
+        text-align: center;
+        color: #ffffff;
+        text-decoration: none;
+        &--active {
+          color: #ffda88;
+        }
+        & + & {
+          margin-left: 36px;
+        }
+      }
+    }
+  }
+</style>
