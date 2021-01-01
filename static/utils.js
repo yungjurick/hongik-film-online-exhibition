83efbbecfb00b22eb43f68ddac2f1ef8f5c5635e
Yung Ju Rick Kim <yungjurick@jaranda.kr>
2021-01-02 03:11:53 +0900

    Fix production cdn url

--- static/utils.js
+++ static/utils.js
@@ -7,7 +7,7 @@ const UTILS = {
   getCdnUrl (directory, endpoint) {
     return location.hostname === 'localhost' || location.hostname === '127.0.0.1'
       ? `https://raw.githubusercontent.com/yungjurick/hongik-film-online-exhibition-cdn/master/${directory}/${endpoint}`
-      : ''
+      : `https://rawcdn.githack.com/yungjurick/hongik-film-online-exhibition-cdn/106a30db94d5e8cad69ac77e483dfbe7243b5c62/${directory}/${endpoint}`
   }
 }
 
